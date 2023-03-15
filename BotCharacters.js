e80ba6363fea00952cbdd562848490428d1ec423
Vihanga-Theekshana <121595586+Vihanga-Theekshana@users.noreply.github.com>
2023-03-15 16:40:34 +0530

    Add files via upload

---
+++ BotCharacters.js
@@ -0,0 +1,294 @@
+/* ---------------------------------------------------------------------------------/
+/                                                                                   /
+/             d8888 888    888                        888b     d888 8888888b.       /
+/            d88888 888    888                        8888b   d8888 888  "Y88b      /
+/           d88P888 888    888                        88888b.d88888 888    888      /
+/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
+/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
+/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
+/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
+/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
+/                                                                                   / 
+/-----------------------------------------------------------------------------------/
+/ Author and Main Developer: FantoX                                                 /
+/ Github: https://github.com/FantoX001/Atlas-MD                                     /
+/ Powered By: Team ATLAS                                                            /
+/-----------------------------------------------------------------------------------/
+/             Meet Team ATLAS who holds all rights to this repository:              /
+/                                                                                   /
+/ 1. Pratyush - https://github.com/pratyush4932                                     /
+/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
+/ 3. Kai - https://github.com/Kai0071                                               /                    
+/ 4. Devime - https://github.com/Devime69                                           /
+/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
+/                                                                                   /
+/ ----------------------------------------------------------------------------------/
+/                                                                                   /
+/      With all of our hard work and defication you can enjoy this awesome bot!     /  
+/                                                                                   / 
+/----------------------------------------------------------------------------------*/
+
+
+
+/* ---Added Anime Character list---  /
+
+ 0 --- Atlas MD
+ 1 --- Power
+ 2 --- Makima
+ 3 --- Denji
+ 4 --- Zero Two
+ 5 --- Chika
+ 6 --- Miku
+ 7 --- Marin Kitagawa
+ 8 --- Ayanokoji
+ 9 --- Ruka
+10 --- Mizuhara
+11 --- Rem
+12 --- Sumi
+13 --- Kaguya
+14 --- Yumeko
+15 --- Kurumi
+16 --- Mai
+17 --- Yor
+18 --- Shinbou
+19 --- Eiko
+
+Image Source: https://wall.alphacoders.com/
+Image Source: https://wallpapercave.com/
+Video Host: https://graph.org/
+
+** Note: 
+Do not change or remove present charID. You can add more characters by adding more charID sequentially.
+Add character name, video link, and image links.
+Don't forget to change "charlist.js" and "setchar.js" insdide "Commands\Mods\"  folder.
+
+*/
+
+
+
+global.charID0 = {
+    botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+    botVideo : "https://graph.org/file/e3b2ee218b98b8679bf98.mp4",
+    botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+    botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+    botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+    botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+    botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+    botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID1 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/b402e2d4718992187787b.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID2 = {
+  botName : "【ᴠɪʜᴀɴɢᴀ ᴛʜᴇᴇᴋꜱʜᴀɴᴀ ᴍᴅ】",
+  botVideo : "https://graph.org/file/de402b3a47195735ac60f.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID3 = {
+  botName : "【ᴠɪʜᴀɴɢᴀ ᴛʜᴇᴇᴋꜱʜᴀɴᴀ ᴍᴅ】",
+  botVideo : "https://graph.org/file/0c48e29b402c76ae6028b.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID4 = {
+  botName : "Dark Alfa MD",
+  botVideo : "https://graph.org/file/04af19c8e2a1a1d1a31b7.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID5 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/140e1f2488a005a889b26.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID6 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/c37d336b8cfad543fd9c8.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID7 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/cf5abde596ccbd6c92c6f.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID8 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/ae503b0700b345816f4d3.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID9 = {
+  botName : "【ᴠɪʜᴀɴɢᴀ ᴛʜᴇᴇᴋꜱʜᴀɴᴀ ᴍᴅ】",
+  botVideo : "https://graph.org/file/b4fe227829affab6bd758.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID10 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/ae039b89dea812fbcf0de.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID11 = {
+  botName : "【ᴠɪʜᴀɴɢᴀ ᴛʜᴇᴇᴋꜱʜᴀɴᴀ ᴍᴅ】",
+  botVideo : "https://graph.org/file/a84a767f4a4d6e8173c7e.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID12 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/d3779cdc930d5914aa305.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID13 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/d9ac8d87ddb9315a0037f.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID14 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/cca5eeed813e151bd2d39.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID15 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/bdb79b42a9c0737730c84.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID16 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/c71c3076de5e792cc209c.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID17 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/8817be3a15ed5c02debf7.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+
+global.charID18 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/1bacc57547657bc93aa66.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
+global.charID19 = {
+  botName : "ƊƋГƘ ѦしẜƋ ⱮƊ",
+  botVideo : "https://graph.org/file/7f99357fec0f000c7ab0a.mp4",
+  botImage1 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage2 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage3 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage4 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage5 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+  botImage6 : "https://i.ibb.co/xYg0JXZ/20230309-231702.jpg",
+}
+
