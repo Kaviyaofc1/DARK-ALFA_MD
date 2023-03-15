15ee984ea49a9d647ff7029638334714777e1b57
Vihanga-Theekshana <121595586+Vihanga-Theekshana@users.noreply.github.com>
2023-03-15 18:58:03 +0530

    Add files via upload

---
+++ Commands/Search/gimage.js
@@ -0,0 +1,47 @@
+const gis = require("g-i-s");
+
+module.exports = {
+  name: "gimage",
+  alias: ["imagesearch", "googleimage", "googleimagesearch", "gig", "gis"],
+  desc: "To get google image search result",
+  category: "Search",
+  usage: `gig <search term>`,
+  react: "⛱️",
+  start: async (Miku, m, { text, prefix, args }) => {
+    if (!args[0])
+      return Miku.sendMessage(
+        m.from,
+        { text: `Please provide a Search Term !` },
+        { quoted: m }
+      );
+    var ImagesearchTerm = args.join(" ");
+
+    gis(args.join(" "), async (error, result) => {
+      n = result;
+      images = n[Math.floor(Math.random() * n.length)].url;
+
+      let resText = `   *『  Image Search Engine  』*\n\n\n_🎀 Search Term:_ *${ImagesearchTerm}*\n`;
+
+      let buttons = [
+        {
+          buttonId: `${prefix}gimage ${ImagesearchTerm}`,
+          buttonText: { displayText: ">>" },
+          type: 1,
+        },
+      ];
+      await Miku.sendMessage(
+        m.from,
+        {
+          image: {
+            url: images,
+          },
+          caption: resText,
+          footer: `*ƊƋГƘ ѦしẜƋ ⱮƊ*`,
+          buttons: buttons,
+          headerType: 4,
+        },
+        { quoted: m }
+      );
+    });
+  },
+};
